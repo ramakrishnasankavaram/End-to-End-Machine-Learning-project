2b8a40e84ee3d1802b064f5b923bcad4c3425883
ramakrishnasankavaram <ramakrishnasankavaram436@gmail.com>
2024-10-31 11:29:39 +0530

    MOdel Trainer

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,6 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
-from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -21,18 +20,12 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train,y_train,X_test,y_test,models,params):
+def evaluate_models(X_train,y_train,X_test,y_test,models):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            para=params[list(models.keys())[i]]
-
-            gs = GridSearchCV(model,para,cv=3)
-            gs.fit(X_train,y_train)
-
-            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
             #model.fit(X_train, y_train)  # Train model
