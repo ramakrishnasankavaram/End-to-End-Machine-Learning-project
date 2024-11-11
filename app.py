cc2643ecd4def3c1bbdea6856a8ce9e0f021d646
ramakrishnasankavaram <ramakrishnasankavaram436@gmail.com>
2024-11-11 13:42:34 +0530

    Added docker file

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
-application=Flask(__name__)
-app=application
+app=Flask(__name__)
 # Route for home page
 @app.route('/')
 def index():
@@ -38,4 +37,4 @@ def predict_datapoint():
         return render_template('home.html',results=results[0])
     
 if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0",debug=True,port=5000)
